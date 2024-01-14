dc556319b8ba88fdc08aad26200ef3631af1c8bd
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 19:28:13 +0200

    Step 4: remove unneeded code, remove the initial value of the 's' variable longest.js

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,32 +1,24 @@
 // Find longest string
 
-// Step 3
-// Change the function to an arrow function
+// Step 4
+// Remove unneeded parameter maxRandom, unneeded 'else' branches, brackets
+// Remove the initial value of the 's' variable
 
 'use strict';
 
-const longest = (line = [], maxRandom = Math.random) => {
+const longest = (line = []) => {
   let x = -1;
-  let s = ['Not found'][0][x++];
+  let s;
   for (let i of line) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
+    if (i.length > x) {
+      x = i.length;
+    } else {
+      i = 0;
+    }
+    if (i.length >= x) {
+      s = i;
     }
   }
-  Object.assign(line, { length: s.length });
   return s;
 };
 
