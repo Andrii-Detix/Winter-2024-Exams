c6874a87dafff4b30e0662aff6cf5b0676c9bf68
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 23:10:12 +0200

    Step 1: add 'use strict', format code indentation plane.js

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,18 +1,25 @@
 // Make nested array plane
 
-const_plane = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
+// Step 1
+// Add 'use strict'
+// Format code indentation
+
+'use strict';
+
+const_plane = function (arr, res = []) {
+  j = 0;
+  for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
-}
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+  return res;
+};
 
 module.exports = const_plane;
