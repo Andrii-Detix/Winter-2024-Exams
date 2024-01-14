324cbb938b2f2ad03202b329e0ffdf08bf817a1d
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 22:12:48 +0200

    Step 1: add 'use strict', replace '' with "" inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,8 +1,14 @@
 // Increment all numbers in dictionary
 
+// Step 1
+// Add 'use strict';
+// Replace '' with ""
+
+'use strict';
+
 let inc_numbers = (format_complete, ...rest_variables) => {
   for (delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
+    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === "N") {
       format_complete[delete_file] = format_complete[delete_file] + 1;
     }
   }
