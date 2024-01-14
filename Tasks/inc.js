0a9fde09ddd791a0d9a75a330478e54efa652968
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 22:42:10 +0200

    Step 5: declare 'result', copy 'obj' to 'result', use 'result' to not mutate incoming parameter inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,17 +1,19 @@
 // Increment all numbers in dictionary
 
-// Step 4
-// Prefer const, minimize let
+// Step 5
+// Declare 'result', copy 'obj' to 'result'
+// Use 'result' instead of 'obj' to not mutate incoming parameter
 
 'use strict';
 
 const incNumbers = (obj) => {
+  let result = structuredClone(obj);
   for (const key in obj) {
-    if ((typeof obj[key]).charAt(0).toUpperCase() === "N") {
-      obj[key] = obj[key] + 1;
+    if ((typeof result[key]).charAt(0).toUpperCase() === "N") {
+      result[key] = result[key] + 1;
     }
   }
-  return obj;
+  return result;
 };
 
 module.exports = incNumbers;
