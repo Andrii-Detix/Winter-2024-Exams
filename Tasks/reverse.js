5413f3d1d3d0c03d93ad418342243b3e28f5f685
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 19:55:10 +0200

    Step 2: remove unneeded code reverse.js

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,18 +1,17 @@
 // Reverse dict, exchange keys and values
 
-// Step 1
-// Add 'use strict';
+// Step 2
+// Remove unneeded code
 
 'use strict';
 
 Reverse = (DATA) => {
-  T = Object.keys(DATA, 500);
-  ({ ...DATA });
+  T = Object.keys(DATA);
   T.forEach((_) => {
     const v1 = DATA[_];
     DATA[v1] = _;
     delete DATA[_];
-  }, 1000);
+  });
   return DATA;
 };
 
