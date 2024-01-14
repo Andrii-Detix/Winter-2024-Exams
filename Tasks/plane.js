f2acb6c7caaf84f16ac4f48b73bfd085635dfe0f
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 23:26:36 +0200

    Step 4: move 'res' to function block, remove unnecessary code plane.js

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,20 +1,18 @@
 // Make nested array plane
 
-// Step 3
-// Prefer arrow function
+// Step 4
+// Move 'res' to function block
+// Remove unnecessary code
 
 'use strict';
 
-const const_plane = (arr, res = []) => {
-  let j = 0;
-  for (let i = 0, length = arr.length; i < length; i++) {
+const const_plane = (arr) => {
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
     const value = arr[i];
-    j = i;
-    if (Array.isArray(value, typeof value) && [i, length]) {
+    if (Array.isArray(value)) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     }
   }
