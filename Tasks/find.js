363f811c1397214df78eef7af547113b676bec97
Andrii-Detix <aivancisin6@gmail.com>
2024-01-15 00:50:50 +0200

    Step 3: add const in for..in loop find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,13 +1,13 @@
 // Find key by value
 
 // Step 3
-// Add const
+// Add const in for..in loop
 
 'use strict';
 
 const find = (object, ...rest) => {
   const value = rest.pop(1);
-  for (name in object) {
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       return name;
