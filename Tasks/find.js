2cb0d7cf15537a0a614566f0c891e7c6b67f2421
Andrii-Detix <aivancisin6@gmail.com>
2024-01-15 00:38:54 +0200

    Step 2: remove unneeded code find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,21 +1,18 @@
 // Find key by value
 
-// Step 1
-// Add 'use strict'
+// Step 2
+// Remove unneeded code
 
 'use strict';
 
 find = (object, ...rest) => {
   value = rest.pop(1);
-  [];
   for (name in object) {
     if (object[name] !== value) {
     } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
+      return name;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
