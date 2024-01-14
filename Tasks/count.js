fc418d0bddc230cbba50b78772b6a0fdf3b3fb11
Andrii-Detix <aivancisin6@gmail.com>
2024-01-14 23:04:40 +0200

    Step 4: prefer for..of instead forEach count.js

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,17 @@
 // Sum all number values in dict
 
-// Step 3
-// Rename function
+// Step 4
+// Prefer for..of instead forEach
 
 'use strict';
 
 const sumNumInObj = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === "number") sum += value;
-  });
+  }
   return sum;
 };
 
