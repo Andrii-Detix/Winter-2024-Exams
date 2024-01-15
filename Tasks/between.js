aa78d79cbb24644786ba8a8e4db05c98feb54df7
Andrii-Detix <aivancisin6@gmail.com>
2024-01-15 08:41:25 +0200

    Step 7: Fix 'startPos' initial value, because the prefix length can be greater than 1 between.js

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,13 +1,14 @@
 // Extract substring between prefix and suffix
 
-// Step 6
-// Remove else branching and variable 'selected'
-// Simplify code
+// Step 7
+// Fix 'startPos' initial value, because the prefix length can be greater than 1
 
 'use strict';
 
 const getValueBetween = (str, prefix, suffix) => {
-  const startPos = str.indexOf(prefix) + 1;
+  const startPos = str.includes(prefix)
+    ? str.indexOf(prefix) + prefix.length
+    : -1;
   const finishPos = str.indexOf(suffix);
   if (startPos === -1 || finishPos === -1) {
     return "";
